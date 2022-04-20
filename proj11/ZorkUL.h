52694a3f87e703b7f36651c9347696d77f1b95e8
EVAN HAWKES <74122496+evanevan-67@users.noreply.github.com>
2022-04-20 15:00:31 +0100

    Update ZorkUL.h

--- proj11/ZorkUL.h
+++ proj11/ZorkUL.h
@@ -11,20 +11,21 @@ using namespace std;
 
 class ZorkUL {
 private:
-	Parser parser;
-	Room *currentRoom;
-	void createRooms();
-	void printWelcome();
-	bool processCommand(Command command);
-	void printHelp();
-	void goRoom(Command command);
+    Parser parser;
+    Room *currentRoom;
+    void createRooms();
+    void printWelcome();
+    bool processCommand(Command command);
+    void printHelp();
+    void goRoom(Command command);
     void createItems();
     void displayItems();
+    string teleport();
 
 public:
-	ZorkUL();
-	void play();
-	string go(string direction);
+    ZorkUL();
+    void play();
+    string go(string direction);
 };
 
 #endif /*ZORKUL_H_*/
